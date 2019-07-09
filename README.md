19d0d2fb44a8d598c03be4a42f5919143585f73d
JiangGua <jonbgua@gmail.com>
2019-07-09 17:45:42 +0800

    将设置项移至config.json，并添加了"图片最小宽度"的参数

--- README.md
+++ README.md
@@ -8,6 +8,15 @@
 ## 使用方法
 
 1. 通过 pip 等方式安装所需库
-2. 在程序根目录新建api.txt，将申请到的Alphacoders API Key存入文件
-3. 按照您的需要在main.py中调整变量 tag_id 和 page 的值
-4. 执行 main.py
+2. 将程序根目录的 `config_sample.json` 重命名为 `config.json`，将申请到的Alphacoders API Key存入文件，并可以自定义一些参数
+3. 按照您的需要在 `main.py` 中调整变量 `tag_id` 和 `page` 的值
+4. 执行 `main.py`
+
+## config 说明
+
+    {
+        "api_key": "",              // 在这里填入你的 API Key
+        "tag_id": "30504",          // 图片们所属的 tag_id
+        "max_page": "2",            // 下载的最大页数
+        "min_width": "1920"         // 图片最小宽度
+    }
--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-tag_id = 30504
-page = 1
+#tag_id = 30504
+#page = 1
 
-with open('api.txt', 'r') as file_obj:
-    api_key = file_obj.read()
+#with open('api.txt', 'r') as file_obj:
+#    api_key = file_obj.read()
 
-for i in range(1, page + 1):
+with open('config.json', 'r') as file_obj:
+    config = file_obj.read()
+    config = json.loads(config)
+    tag_id = str(config['tag_id'])
+    max_page = int(config['max_page'])
+    api_key = str(config['api_key'])
+    min_width = int(config['min_width'])
+
+print(config)
+
+for i in range(1, max_page + 1):
     info = requests.get('https://wall.alphacoders.com/api2.0/get.php?auth=%s&method=tag&id=%s&page=%d&sort=views' % 
                         (api_key, tag_id, i))
     info = json.loads(info.text)
@@ -16,6 +26,11 @@ for i in range(1, page + 1):
     if info['success']:
         for j in range(len(info['wallpapers'])):
             pic_info = info['wallpapers'][j]
+
+            # 若当前图片小于设定的min_width，则直接下一张
+            if int(pic_info['width']) < min_width:
+                continue
+
             file_name = pic_info['id'] + '.' + pic_info['file_type']
             pic = requests.get(pic_info['url_image'])
 
